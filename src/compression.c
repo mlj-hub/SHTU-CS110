fcead92d3985e3f39be090ade9c751db15e04d1f
Jiadong Tu <tujd@shanghaitech.edu.cn>
2022-03-07 21:58:08 +0800

    Initial Framework Commit
    Co-authored-by: Hongdi Yang <yanghd@shanghaitech.edu.cn>

---
+++ src/compression.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "compression.h"
+#include "utils.h"
+
+/* Your code here... */
+
---
+++ src/utils.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "utils.h"
+
+/* Your code here... */
+
+ 
