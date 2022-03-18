e1938929ec9f664060b3f76f46d8f7684e001cf2
Linjie Ma <malj@shanghaitech.edu.cn>
2022-03-18 22:54:56 +0800

    feature: add interface funciton
    1.finish format_compressible check interface
    2.add some examples for compress and check

--- src/compression.c
+++ src/compression.c
@@ -1,9 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "compression.h"
 #include "utils.h"
+
+void R_compress(cmd_info_t * cmd_info){
+    (void) cmd_info;
+}
+
+cmd_state_t R_check(uint32_t cmd){
+    (void) cmd;
+    return 0;
+}
+
+void handle_compressible(cmd_info_t * cmd_info){
+    (void) cmd_info;
+}
+
+void handle_unsure(cmd_info_t * cmd_info){
+    (void) cmd_info;
+}
+
+void format_compressible_check(cmd_info_t * cmd_info,uint32_t * cmd_data){
+    uint32_t i =0;
+    for(i=0;i<cmd_num;i++){
+        cmd_info[i].cmd = cmd_data[i];
+        switch(cmd_data[i] & OPCODE){
+            case R:
+                cmd_info[i].format = R;
+                cmd_info[i].state = R_check(cmd_info[i].cmd);
+                break;
+            case I:
+                cmd_info[i].format = I;
+                break;
+            case LI:
+                cmd_info[i].format = LI;
+                break;
+            case U:
+                cmd_info[i].format = U;
+                break;
+            case S:
+                cmd_info[i].format = S;
+                break;
+            case SB:
+                cmd_info[i].format = SB;
+                break;
+            case UJ:
+                cmd_info[i].format = UJ;
+                break;
+            case JI:
+                cmd_info[i].format = JI;
+                break;
+            default:
+                cmd_info[i].format = UNVAILD;
+        }
+    }
+    return ;
+}
+
+cmd_info_t * compress(uint32_t * cmd_data){
+    cmd_info_t * cmd_info;
+    cmd_info = (cmd_info_t *)calloc(sizeof(cmd_info_t),cmd_num);
+
+    format_compressible_check(cmd_info,cmd_data);
+    free(cmd_data);
+    return NULL;
+}
 
 /* Your code here... */
 
--- src/compression.h
+++ src/compression.h
@@ -2,5 +2,35 @@
 #define COMPRESSION_H
 
 /* Your code here... */
+#define OPCODE 0x7f
 
+typedef enum{
+    R  = 0x33,
+    I  = 0x13,
+    LI = 0x03,
+    S  = 0x23,
+    SB = 0x63,
+    JI = 0x67,
+    U  = 0x37,
+    UJ = 0x6f,
+    UNVAILD = 0x00
+}RISCV_Format_e;
+
+typedef enum{
+    UNSURE,
+    COMPRESSIBLE,
+    INCPMPRESSIBLE
+}cmd_state_t;
+
+typedef struct{
+    uint32_t cmd;
+    cmd_state_t  state;
+    RISCV_Format_e format;
+}cmd_info_t;
+
+#define REGISTER 0x1f;
+#define FUNCT3 0x07
+#define FUNCT7 0x7f
+
+cmd_info_t * compress(uint32_t *);
 #endif
