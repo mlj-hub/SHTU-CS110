a41991f7f25c19095d6eb58514eec46a95b5896f
Linjie Ma <malj@shanghaitech.edu.cn>
2022-06-06 09:09:55 +0800

    draw score, ground, and trex

--- include/game_config.h
+++ include/game_config.h
@@ -1,8 +1,41 @@
 #ifndef _GAME_CONFIG_H_
 #define _GAME_CONFIG_H_
+#include "lcd/lcd.h"
+#include <stdint.h>
 
 #define TREX_START_Y  40
 #define TREX_END_Y  59
 #define GROUND_START_Y 60
+#define OBSTACLE_START_Y 40
+#define OBSTACLE_END_Y  59
+#define SCORE_START_X  150
+#define SCORE_START_Y 10
+
+typedef enum{
+    MOVE0,
+    MOVE1,
+    SQUAT0,
+    SQUAT1,
+    JUMP
+}trex_state_e;
+
+typedef struct{
+    trex_state_e state;
+    u16 start_x;
+    u16 start_y;
+    u16 end_x;
+    u16 end_y;
+}trex_t;
+
+typedef enum{
+    INIT,
+    PLAYING,
+    OVER,
+}game_state_e;
+
+typedef struct{
+    int32_t score;
+    game_state_e state;
+}game_t;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include "lcd/lcd.h"
 #include <string.h>
 #include "utils.h"
+#include "img.h"
+#include "game_config.h"
+
+game_t game;
+trex_t trex;
 
 void Inp_init(void)
 {
@@ -23,13 +28,76 @@ void IO_init(void)
     Lcd_Init(); // LCD init
 }
 
-void draw_deno(){
+void update_ground(){
+
+}
+
+void update_trex(){
+
+}
+
+void update_obstacle(){
+
+}
+
+void update_score(){
+    int cnt=1;
+    int32_t temp=game.score;
+    while(temp%10!=temp){
+        cnt++;
+        temp/=10;
+    }
+    LCD_ShowNum(SCORE_START_X-cnt*8,SCORE_START_Y,game.score,cnt,WHITE);
+}
+
+void update(){
     
 }
+
+void get_state(){
+
+}
+
+
 
 int main(void)
 {
     IO_init();         // init OLED
     // YOUR CODE HERE
+    LCD_Clear(BLACK);
 
+    int g1_start_x=0,g2_start_x=160;
+    int show_trex = 0;
+    int32_t score=0;
+    delay_1ms(100);
+    while(1){
+
+        if(show_trex==8){
+            LCD_ShowPic(10,40,29,59,trex1);
+        }
+        if(show_trex==16){
+            LCD_ShowPic(10,40,29,59,trex2);
+        }
+        if(g1_start_x==0){
+            LCD_ShowPicture_Part(0,GROUND_START_Y,g1,160-g2_start_x,0,160,10,160);
+            LCD_ShowPicture_Part(g2_start_x,GROUND_START_Y,g2,0,0,160-g2_start_x,10,160);
+            g2_start_x--;
+            if(g2_start_x==0)
+                g1_start_x = 160;
+        }
+        if(g2_start_x==0){
+            LCD_ShowPicture_Part(0,GROUND_START_Y,g2,160-g1_start_x,0,160,10,160);
+            LCD_ShowPicture_Part(g1_start_x,GROUND_START_Y,g1,0,0,160-g1_start_x,10,160);
+            g1_start_x--;
+            if(g1_start_x==0)
+                g2_start_x = 160;
+        }
+        if(show_trex<16)
+            show_trex++;
+        else
+            show_trex=0;
+        delay_1ms(50);
+        game.score++;
+        update_score();
+    }
 }
